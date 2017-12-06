fdc7ec9970488dc8634eee73048ad396d08781e2
usera <usera@gittraining.com>
2017-12-05 23:50:01 -0500

    changed function name from w_strlen to my_wc_strlen

--- src/string_operations.c
+++ src/string_operations.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <wchar.h>
-size_t w_strlen(const wchar_t *s)
+/*widw charater support function */
+size_t my_wc_strlen(const wchar_t *s)
 {
    const wchar_t *p = s;
    while (*p)
